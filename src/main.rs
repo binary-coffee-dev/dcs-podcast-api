f9ca17462663299ae19b8a0545188e114e3ec57c
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-07-22 01:15:38 +0200

    Setup default endpoint and initial response

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,27 @@
 
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use serde::Serialize;
+use rocket_contrib::json::Json;
+
+#[derive(Serialize)]
+struct Poscast {
+    id: String,
+    name: String,
+    url: String,
+}
+
+#[get("/podcasts")]
+fn todo() -> Json<Vec<Poscast>> {
+    let mut podcast_list = Vec::new();
+    podcast_list.push(Poscast {
+        id: String::from("asdf234"),
+        name: String::from("asdf234"),
+        url: String::from("asdf234")
+    });
+    Json(podcast_list)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![todo]).launch();
 }
