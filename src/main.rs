b156c92457b49ea217994f966886b534e82b5928
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-08-18 17:50:33 +0200

    Integrate cors to the application

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,33 @@
 
 mod libs;
 
+use rocket_cors;
+
+use rocket::http::Method;
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Error};
+
 // use async_std::task::block_on;
 use futures::executor::block_on;
 use libs::{database_client::{DatabaseClient, DatabaseBase}, routes};
 
-fn main() {
+fn main() -> Result<(), Error> {
     let mut database = DatabaseClient::new();
     block_on(database.connect()).expect("The database connection fails");
 
+    let allowed_origins = AllowedOrigins::all();
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get, Method::Post, Method::Put].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: false,
+        ..Default::default()
+    }.to_cors()?;
+
     rocket::ignite()
         .manage(database)
-        .mount("/", routes::initialize_routes()).launch();
+        .mount("/", routes::initialize_routes())
+        .attach(cors)
+        .launch();
+
+    Ok(())
 }
