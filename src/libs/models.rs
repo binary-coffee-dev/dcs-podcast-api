4fda1266d12a7367782eb0026f243cabef996cde
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-08-18 18:58:12 +0200

    Serialize podcast id to string

--- src/libs/models.rs
+++ src/libs/models.rs
@@ -1,12 +1,33 @@
-use serde::{Serialize, Deserialize};
+use serde::{Serialize, Deserialize, Serializer, ser::SerializeStruct};
 use mongodb::bson::oid::ObjectId;
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct Podcast {
     pub _id: Option<ObjectId>,
     pub name: Option<String>,
     pub banner: Option<String>,
     pub url: Option<String>,
+}
+
+impl Serialize for Podcast {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("Podcast", 4)?;
+        match &self._id {
+            Some(id) => {
+                state.serialize_field("id", &id.to_string())?
+            }
+            None => {
+                state.serialize_field("id", &self._id)?;
+            }
+        }
+        state.serialize_field("name", &self.name)?;
+        state.serialize_field("banner", &self.banner)?;
+        state.serialize_field("url", &self.url)?;
+        state.end()
+    }
 }
 
 impl Default for Podcast {
